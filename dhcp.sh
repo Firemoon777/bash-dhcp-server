de174ce230d4a53b2b6d165c72ed8b9bc7de8bbc
Firemoon777 <firemoon@icloud.com>
2018-12-27 15:49:34 -0500

    DISCOVER reading implemented

--- dhcp.sh
+++ dhcp.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-nc -l 0.0.0.0 -up 67 | (
-read -n1 op
-echo "op: $op"
+nc -l 0.0.0.0 -up 67 | stdbuf -o0 od -v -w1 -t u1 -An | (
+	msg=()
+	for i in {0..239}; do
+		read -r tmp
+		msg[$i]=$tmp
+	done
+
+	echo ${msg[*]}
+
+	if [[ "${msg[236]}${msg[237]}${msg[238]}${msg[239]}" == "991308399" ]]; then
+		op=0
+		while [[ $op -ne 255 ]]; do
+			read -r op
+			read -r len
+			echo "op: $op, len $len"
+			if [[ $len > 0 ]]; then
+				printf '\t'
+				for i in $(seq 0 $(($len-1))); do
+					read -r data
+					printf "%x " $data
+				done
+				echo
+			fi
+		done
+	fi
 )
